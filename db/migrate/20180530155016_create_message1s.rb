b2ce96c7e583c1824b9dd8cfe5c099261f0333c7
Ethan <equigsrus@gmail.com>
2018-06-04 07:55:10 -0400

    Added pg and 12factor gems for heroku

--- db/migrate/20180530155016_create_message1s.rb
+++ db/migrate/20180530155016_create_message1s.rb
@@ -1,6 +1,6 @@
 class CreateMessage1s < ActiveRecord::Migration[5.0]
   def change
-    create_table :message1s do |t|
+    create_table :messages1 do |t|
     t.text :content
       t.timestamps
     end
